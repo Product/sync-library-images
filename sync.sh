b6f84e4be2e8d4f78018f53bb6681cc978d81b93
SEC <sec@SECdeMBP.sec.dosec>
2022-05-20 13:08:49 +0800

    update

--- sync.sh
+++ sync.sh
@@ -30,11 +30,7 @@ diff_images() {
     CURRENT_COMMIT=$(git log -1 upstream/master --format='%H')
     echo ${CURRENT_COMMIT}
     echo "--ok1--"
-    git tag -l
-    echo "-------------"
-    git tag -l | egrep --only-matching -E '^([[:digit:]]{12})'
-    LAST_TAG=$(git tag -l | egrep --only-matching -E '^([[:digit:]]{12})' | sort -nr | head -n1)
-    echo ${LAST_TAG}
+    LAST_TAG=$(git tag -l | egrep --only-matching -E '^([[:digit:]]{12})' | sort -nr | head -n1  || true )
     echo "--ok1-1--"
     : ${LAST_TAG:=$(git log upstream/master --format='%H' | tail -n1)}
     echo "--ok2--"
