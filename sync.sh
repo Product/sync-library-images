10e3a42d389858afcfac4af5479b12144b9db20f
Product <snortguize@163.com>
2022-05-20 20:12:49 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -60,10 +60,10 @@ sync_images() {
     TOTAL_NUMS=$(echo -e ${IMAGES} | tr ' ' '\n' | wc -l)
     for image in ${IMAGES}; do
         echo ${image}
-        if skopeo inspect docker://${REGISTRY_LIBRARY}/${name}:${tags} --raw | jq '.' | grep "schemaVersion";then
-            echo "---the images  ${REGISTRY_LIBRARY}/${name}:${tags} has exists , skipping --- "
-            continue
-        fi
+        #if skopeo inspect docker://${REGISTRY_LIBRARY}/${name}:${tags} --raw | jq '.' | grep "schemaVersion";then
+        #    echo "---the images  ${REGISTRY_LIBRARY}/${name}:${tags} has exists , skipping --- "
+        #    continue
+        #fi
         let CURRENT_NUM=${CURRENT_NUM}+1
         echo -e "$YELLOW_COL Progress: ${CURRENT_NUM}/${TOTAL_NUMS} $NORMAL_COL"
         name="$(echo ${image} | cut -d ':' -f1)"
